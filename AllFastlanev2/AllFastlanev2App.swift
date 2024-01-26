be866faa412c9fbf8e75e54d08c3178bf8cfb411
Ignacio Arias <1992ignacio@gmail.com>
2024-01-26 01:03:41 -0800

    Initial Commit

---
+++ AllFastlanev2/AllFastlanev2App.swift
@@ -0,0 +1,17 @@
+//
+//  AllFastlanev2App.swift
+//  AllFastlanev2
+//
+//  Created by Ignacio Arias on 2024-01-26.
+//
+
+import SwiftUI
+
+@main
+struct AllFastlanev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
